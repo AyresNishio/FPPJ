2e29f623435f721f0cbd770f126d963489c0288c
Ayres Nishio <ayresnishio@gmail.com>
2021-07-23 12:35:09 -0300

    Inicio

---
+++ FPNR.py
@@ -0,0 +1,15 @@
+from sub.deltaz import *
+from sub.flowres import *
+from sub.jacob import *
+from sub.ler_dados import *
+from sub.output import *
+from sub.solve import *
+import pandas as pd
+
+# def FPNR():
+#     #Leitura de dados da rede e das cargas (subrotina ler_dados)
+
+line, z, nbus, nlin = lerdados('NetData14Bus.xlsx','LoadData14Bus.xlsx')
+
+# if __name__ == "FPNR":
+#     FPNR()
