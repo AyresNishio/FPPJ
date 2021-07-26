8ba76355904dcf2aa02d575e2eb64add8d68cb0d
Ayres Nishio <ayresnishio@gmail.com>
2021-07-26 17:08:36 -0300

    Processo Iterativo

---
+++ sub/flowres.py
@@ -0,0 +1,14 @@
+
+def flowres(vmag, vang, Ybarra, G, B, Z, line, nbus, nlin):
+
+    FPA=0
+    FPA1=0
+    FPR=0
+    FPR1=0
+    IPA=0
+    IPR=0
+    Ibarra=0
+    FC=0 
+    FC1=0
+
+    return FPA, FPA1, FPR, FPR1, IPA, IPR, Ibarra, FC, FC1
